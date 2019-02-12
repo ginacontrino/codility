2758e8b43b4790cac263500b9a800c2f0e15966a
ginacontrino <klein_rosi@hotmail.de>
2019-02-12 20:08:08 +0100

    Implement lesson 3: cyclic rotation

---
+++ lesson03/cyclic_rotation.py
@@ -0,0 +1,19 @@
+
+def solution(array, rotations):
+ arrayLength = len(array)
+
+ if arrayLength == 0:
+ 	return []
+
+ rotations = (rotations + arrayLength) % arrayLength
+ if rotations == 0 or arrayLength == 0 or arrayLength == rotations:
+  return array
+
+ newArray = []
+ for i in range(arrayLength):
+  newPosition = (i + rotations) % arrayLength
+  newArray.insert(newPosition, array[i])
+ 
+ return newArray	
+   
+print solution([3, 8, 9, 7, 6] , 3)
