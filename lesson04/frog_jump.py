591215b2d0a9e6bdb4f43f770eea4aa05858f0b2
ginacontrino <klein_rosi@hotmail.de>
2019-02-13 20:54:04 +0100

    Implement lesson 4: frog jump

---
+++ lesson04/frog_jump.py
@@ -0,0 +1,11 @@
+import math
+
+def solution(initalPosition, goalPosition, jumpDistance):
+ if initalPosition == goalPosition or jumpDistance == 0:
+ 	return 0
+
+ return math.ceil((goalPosition - initalPosition)/float(jumpDistance))
+ 
+
+print solution(3, 999111321, 7)
+
