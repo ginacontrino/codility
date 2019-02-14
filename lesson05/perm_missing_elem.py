938caee236d09007f40d4f9791273dc9ce623ade
ginacontrino <klein_rosi@hotmail.de>
2019-02-14 23:10:31 +0100

    Implement lesson 5: perm missing element

---
+++ lesson05/perm_missing_elem.py
@@ -0,0 +1,7 @@
+from functools import reduce
+
+def solution(array):
+ completeArray = list(range(1, len(array)+2))
+ return reduce((lambda x, y: x^y), array + completeArray)
+
+print solution([2,3,5,1])
